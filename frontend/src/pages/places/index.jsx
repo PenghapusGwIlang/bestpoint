4efc3e17125798d43cc3ada9ec304be4169361ff
Muhammad Naufal <naufalmhd24@gmail.com>
2023-09-17 20:12:40 +0700

    added an useEffect to fetch place API from backend

--- frontend/src/pages/places/index.jsx
+++ frontend/src/pages/places/index.jsx
@@ -1,10 +1,33 @@
-import CompNavbar from '../../components/Navbar'
+import { useEffect, useState } from 'react'
 
 function Index() {
+  const [places, setPlaces] = useState(null)
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      const response = await fetch('http://localhost:3000/api/v1/places')
+      const data = await response.json()
+
+      if (response.ok) {
+        setPlaces(data)
+      }
+    }
+
+    fetchPlaces()
+  }, [])
+
   return (
     <>
-      <main class="container mt-5">
+      <main className="container mt-5">
         <h1>All Places</h1>
+        <div className="places">
+          {places &&
+            places.map((place) => (
+              <p key={place._id}>
+                <a href={`places/${place._id}`}>{place.title}</a>
+              </p>
+            ))}
+        </div>
       </main>
       {/* <div id="mapContainer" style={'width: 100%; height: 100vh'}></div> */}
     </>
